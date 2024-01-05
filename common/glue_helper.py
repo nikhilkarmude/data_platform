2f973ddc9e65b89a30078167be1e06232da550e5
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-01-05 18:29:07 -0500

    push

--- common/glue_helper.py
+++ common/glue_helper.py
@@ -29,6 +29,43 @@ class GlueHelper:
         response = self.client.get_job_run(JobName=job_name, RunId=job_run_id)
         return response['JobRun']['JobRunState']
 
+
+    def upsert_job(self, job_name, script_location, role, args):
+        try: 
+            response = self.client.create_job(Name=job_name, Role=role, Command={'Name': 'glueetl', 'ScriptLocation': script_location}, DefaultArguments=args)
+            print('Job Created')
+        except self.client.exceptions.AlreadyExistsException:
+            response = self.update_job(job_name, script_location, role, args)
+            print('Job Updated')
+        return response
+    
+    def create_job(self, job_name, script_location, role, args, worker_type, number_of_workers):
+        response = self.client.create_job(
+            Name=job_name,
+            Role=role,
+            Command={'Name': 'glueetl', 'ScriptLocation': script_location},
+            DefaultArguments=args,
+            WorkerType=worker_type,
+            NumberOfWorkers=number_of_workers
+        )
+        return response
+    
+    def update_job(self, job_name, script_location, role, args, worker_type, number_of_workers):
+        response = self.client.update_job(
+            JobName=job_name,
+            JobUpdate={
+                'Role': role,
+                'Command': {'Name': 'glueetl', 'ScriptLocation': script_location}, 
+                'DefaultArguments': args,
+                'WorkerType': worker_type,
+                'NumberOfWorkers': number_of_workers
+                }
+        )
+        return response
+
+#helper.create_job('my_job', 's3://mybucket/myscript.py', 'myRole', args, 'G.1X', 10)
+
+
 # Usage:
 
 helper = GlueHelper()
