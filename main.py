def9bbe055bb78bbc546b1008565c8e537e34572
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-01-18 12:47:31 -0500

    test

--- main.py
+++ main.py
@@ -94,3 +94,46 @@ def split_large_json_file(bucket_name, large_file_path, job_name, num_records, s
 # call the function with required parameters
 split_large_json_file(bucket_name='mybucket', large_file_path='large-file.json', job_name='myjob', num_records=5000, small_file_path='small-file.json')
 
+
+
+def split_large_txt_file(bucket_name, large_file_path, job_name, num_records, small_file_path):
+    job = Job(glueContext)
+    job.init(job_name, args)
+
+    # Read the large text file directly from S3 bucket into a DynamicFrame
+    # Although we're reading a text file, we use format='csv' here as Glue doesn't have a specific 'txt' format. 
+    # 'csv' is a versatile format option in Glue which allows us to process structured text files with different delimiters. 
+    # The format_options={'withHeader': True, 'separator': '\t'} line sets the options specific to 'csv', where 'separator' can be adjusted to match the delimiter in your text file.
+    txt_dyf = glueContext.create_dynamic_frame.from_options(
+        's3',
+        {'paths': [f's3://{bucket_name}/{large_file_path}']},
+        format='csv',
+        format_options={'withHeader': True, 'separator': '\t'},
+        transformation_ctx='txt_dyf'
+    )
+
+    num_partitions = int(txt_dyf.count() / num_records)
+    if txt_dyf.count() % num_records != 0:
+        num_partitions += 1
+
+    # Convert DynamicFrame to DataFrame and Repartition
+    txt_df = txt_dyf.toDF()
+    txt_df = txt_df.repartition(num_partitions)
+
+    # Convert DataFrame back to DynamicFrame 
+    rebinned_dyf = DynamicFrame.fromDF(txt_df, glueContext, "rebinned_dyf")
+
+    # Write the partitions as separate text files back to S3
+    # When writing the data back to S3, we also use format='csv' and specify a '\t' separator to write it as a TXT/TSV file.
+    glueContext.write_dynamic_frame.from_options(
+        frame = rebinned_dyf,
+        connection_type = "s3",
+        connection_options = {"path": f"s3://{bucket_name}/{small_file_path}"},
+        format_options={"quoteChar": '-', "escapeChar": '-', "separator": '\t'},
+        format = "csv"
+    )
+
+    job.commit()
+
+# call the function with required parameters
+split_large_txt_file(bucket_name='mybucket', large_file_path='large-file.txt', job_name='myjob', num_records=5000, small_file_path='small-file.txt')
