d8df110e02a2cfb8931ad05a8762a86d6b377c33
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-02-15 16:06:47 -0500

    snowflake

--- framework/snowflake.py
+++ framework/snowflake.py
@@ -1,126 +1,146 @@
-import os, sys
-# below lines are added so that we can run this file as - python ingestion.py
-# without facing module not found error.
-# If these lines are removed, then below import statements need to be modified.
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(current_dir)
-sys.path.append(project_root)
-
-
-import snowflake.connector
+from sqlalchemy import create_engine, text
+from snowflake.sqlalchemy import URL
+import pandas as pd
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+class SnowflakeDB:
+    """This class provides methods to perform operations on a Snowflake database."""
 
+    def __init__(self, user, password, account, warehouse, database):
+        """
+        Initialize connection parameters for the Snowflake database.
 
-class SnowflakeDatabase:
+        Parameters:
+        user (str): The Snowflake username.
+        password (str): The password for the Snowflake user.
+        account (str): The Snowflake account in the format 'xy12345.west-europe.azure'.
+        database (str): The name of the Snowflake database.
+        warehouse (str): The name of the Snowflake warehouse.
 
-    def __init__(self, user, password, account, warehouse, database, schema):
-        """Initialize connection parameters and connect to the Snowflake DB."""
+        Example Usage:
+        db = SnowflakeDB('<username>', '<password>', '<account>', '<warehouse>', '<database>')
+        """
         self.user = user
         self.password = password
         self.account = account
         self.warehouse = warehouse
         self.database = database
-        self.schema = schema
-        self.connection = self.connect()
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Database connection parameters initialization complete.")
 
-    def connect(self):
-        """Establish and return a connection with the Snowflake DB."""
-        logger.info("Connecting to Snowflake database")
-        return snowflake.connector.connect(
+    def get_engine(self):
+        """
+        Establish and return an engine to the Snowflake database.
+
+        Returns:
+        sqlalchemy.engine.Engine: SQLAlchemy engine instance.
+
+        Example Usage:
+        engine = db.get_engine()
+        """
+        connection_string = URL(
             user=self.user,
             password=self.password,
             account=self.account,
             warehouse=self.warehouse,
-            database=self.database,
-            schema=self.schema)
+            database=self.database
+        )
+        self.logger.info("Snowflake Database connection established.")
+        return create_engine(connection_string)
 
-    def select(self, query):
-        """Perform a SELECT query and return results.
+    def select_data(self, schema_name: str, table_name: str, where_conditions: dict = None) -> pd.DataFrame:
+        """
+        Fetches and returns rows from the specified table that match the 'where' condition.
+
         Parameters:
-            query (str): The SQL query to execute.
+        schema_name (str): Name of the schema in which the table is located.
+        table_name (str): Name of the table from which to fetch data.
+        where_conditions (dict): Dictionary with column names as keys and corresponding values as conditions to select rows.
+
         Returns:
-            list: The query result as a list of tuples.
-        """
-        logger.info("Fetching data from database")
-        cur = self.connection.cursor()
-        cur.execute(query)
-        rows = cur.fetchall()
-        cur.close()
-        return rows
+        pandas.DataFrame: DataFrame containing fetched data.
 
-    def insert(self, query, data):
-        """Perform an INSERT operation.
+        Example Usage Without WHERE condition:
+        results = db.select_data("MY_SCHEMA", "MY_TABLE")
+
+        Example Usage WITH WHERE condition:
+        where_conditions = {
+            "SYSTEM_NAME": "Test System",
+            "SYSTEM_INSTANCE": "1"
+        }
+        results = db.select_data("MY_SCHEMA", "MY_TABLE", where_conditions)
+        """
+        self.logger.info(f"Fetching data from Snowflake table: {table_name}")
+        query = f'SELECT * FROM "{schema_name}"."{table_name}"'
+        if where_conditions:
+            conditions_str = ' AND '.join([f'"{k}" = \'{v}\'' for k, v in where_conditions.items()])
+            query += f' WHERE {conditions_str}'
+        df = pd.read_sql_query(query, self.get_engine())
+        self.logger.info(f"Fetched {len(df)} rows from Snowflake table: {table_name}")
+        return df
+
+    def insert_data(self, schema_name: str, table_name: str, data: list) -> None:
+        """
+        Inserts new rows into the specified table with provided data.
+
         Parameters:
-            query (str): The SQL query to execute.
-            data (list): The data to be inserted.
-        """
-        logger.info("Inserting data into database")
-        cur = self.connection.cursor()
-        cur.execute(query, data)
-        self.connection.commit()
-        cur.close()
+        schema_name (str): Name of the schema in which the table is located.
+        table_name (str): Name of the table to insert data into.
+        data (list): List of dictionaries containing column names as keys and corresponding data as values.
+                     Each dictionary represents a new row to be inserted.
 
-    def update(self, query):
-        """Perform an UPDATE operation.
+        Example Usage:
+        data = [
+            {
+                "COL1": "value1",
+                "COL2": "value2",
+                "COL3": "value3"
+            },
+            {
+                "COL1": "value4",
+                "COL2": "value5",
+                "COL3": "value6"
+            }
+        ]
+        db.insert_data("MY_SCHEMA", "MY_TABLE", data)
+        """
+        self.logger.info(f"Inserting data into Snowflake table: {table_name}")
+        with self.get_engine().begin() as connection:
+            for row in data:
+                keys_list = ', '.join([f'"{k}"' for k in row.keys()])
+                values_list = ', '.join([f"'{v}'" for v in row.values()])
+                query = f'INSERT INTO "{schema_name}"."{table_name}" ({keys_list}) VALUES ({values_list})'
+                connection.execute(text(query))
+        self.logger.info(f"Inserted {len(data)} rows into Snowflake table: {table_name}")
+
+    def update_data(self, schema_name: str, table_name: str, set_values: dict, where_conditions: dict) -> None:
+        """
+        Updates rows in the specified table that match the 'where' conditions.
+        Columns to be updated and their new values are provided in the 'set_values' dictionary.
+
         Parameters:
-            query (str): The SQL query to execute.
+        schema_name (str): Name of the schema in which the table is located.
+        table_name (str): Name of the table to update.
+        set_values (dict): Dictionary that contains column names as keys and the new values to update.
+        where_conditions (dict): Dictionary with column names as keys and corresponding values as conditions to select rows.
+
+        Example Usage:
+        set_values = {
+            "COL1": "newvalue1",
+            "COL2": "newvalue2",
+            "COL3": "newvalue3"
+        }
+        where_conditions = {
+            "COL1": "value1",
+            "COL2": "value2",
+            "COL3": "value3"
+        }
+        db.update_data("MY_SCHEMA", "MY_TABLE", set_values, where_conditions)
         """
-        logger.info("Updating data in database")
-        cur = self.connection.cursor()
-        cur.execute(query)
-        self.connection.commit()
-        cur.close()
-
-    def delete(self, query):
-        """Perform a DELETE operation.
-        Parameters:
-            query (str): The SQL query to execute.
-        """
-        logger.info("Deleting data from database")
-        cur = self.connection.cursor()
-        cur.execute(query)
-        self.connection.commit()
-        cur.close()
-
-    def execute_procedure(self, proc_name, *args):
-        """Execute a stored procedure with given arguments.
-        Parameters:
-            proc_name (str): The name of the stored procedure.
-            *args: Variable length argument list for stored procedure parameters.
-        """
-        logger.info("Running stored procedure")
-        cur = self.connection.cursor()
-        call = "CALL {}({})".format(proc_name, ','.join(['%s'] * len(args)))
-        cur.execute(call, tuple(args))
-        self.connection.commit()
-        cur.close()
-
-    def close(self):
-        """Close the connection with the Snowflake DB."""
-        logger.info("Closing connection to database")
-        self.connection.close()
-
-
-
-# db = SnowflakeDatabase(user='username', password='password', account='account_url', warehouse='warehouse', database='database', schema='public')
-
-# # SELECT
-# rows = db.select("SELECT * FROM table")
-# print(rows)
-
-# # Insert
-# db.insert("INSERT INTO table (column) VALUES (%s)", ['value'])
-
-# # Update
-# db.update("UPDATE table SET column = 'new_value' WHERE column = 'old_value'")
-
-# # Delete
-# db.delete("DELETE FROM table WHERE column = 'value'")
-
-# # Stored Procedure
-# db.execute_procedure('stored_proc', 'param1', 'param2', 'param3')
-
-# db.close()
+        self.logger.info(f"Updating the data in the Snowflake table: {table_name}")
+        set_clause = ', '.join([f'"{k}" = \'{v}\'' for k, v in set_values.items()])
+        where_clause = ' AND '.join([f'"{k}" = \'{v}\'' for k, v in where_conditions.items()])
+        query = f'UPDATE "{schema_name}"."{table_name}" SET {set_clause} WHERE {where_clause}'
+        with self.get_engine().begin() as connection:
+            connection.execute(text(query))
+        self.logger.info(f"Data updated in the Snowflake table: {table_name}")
