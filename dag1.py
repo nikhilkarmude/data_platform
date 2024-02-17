4041470058d448e77cd2c0419ae364fa7e9ef159
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-02-17 11:17:37 -0500

    snowflake

--- dag1.py
+++ dag1.py
@@ -1,21 +1,37 @@
+from datetime import timedelta, datetime
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-import pkg_resources
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
 
-def print_packages():
-    packages = [d for d in pkg_resources.working_set]
-    for package in packages:
-        print(package)
-        
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
 with DAG(
-    "print_packages",
-    start_date=datetime(2022, 1, 1),
-    schedule_interval=None,
-    catchup=False,
+    'dir_structure_dag',
+    default_args=default_args,
+    description='A simple directory structure printing DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['example'],
 ) as dag:
-
-    print_packages = PythonOperator(
-        task_id="print_packages",
-        python_callable=print_packages,
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id='print_cwd',
+        bash_command='echo "Current Working Directory: $(pwd)"',
     )
+
+    t2 = BashOperator(
+        task_id='print_structure',
+        bash_command='echo "Directory Structure:" && find /path/to/your/directory -print', # Replace '/path/to/your/directory' with the directory you want to print
+    )
+
+t1 >> t2
