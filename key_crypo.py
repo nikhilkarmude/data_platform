a32a0989fb46bbeaf3f101c24b7549bdf74926df
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-03-13 19:04:53 -0400

    test

--- key_crypo.py
+++ key_crypo.py
@@ -1,39 +1,45 @@
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
 
 class AsymmetricCipher:
-    def __init__(self):
-        self.private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
-        self.public_key = self.private_key.public_key()
+    def __init__(self, private_key=None, public_key=None):
+        if private_key and public_key:
+            self._load_keys(private_key, public_key)
+        else:
+            self._generate_keys()
+        
         self.padding = padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
+
+    def _load_keys(self, private_key, public_key):
+        self.private_key = serialization.load_pem_private_key(private_key, None)
+        self.public_key = serialization.load_pem_public_key(public_key)
+        
+    def _generate_keys(self):
+        self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        self.public_key = self.private_key.public_key()
         
     def encrypt(self, message):
-        encrypted = self.public_key.encrypt(
-            message,
-            self.padding
-        )
+        encrypted = self.public_key.encrypt(message, self.padding)
         return encrypted
-        
+
     def decrypt(self, encrypted):
-        original_message = self.private_key.decrypt(
-            encrypted,
-            self.padding
-        )
+        original_message = self.private_key.decrypt(encrypted, self.padding)
         return original_message
 
-# usage example
-cipher = AsymmetricCipher()
-message = b'A secret message'
-encrypted = cipher.encrypt(message)
-print('Encrypted message:', encrypted)
-decrypted = cipher.decrypt(encrypted)
-print('Decrypted message:', decrypted)
+    def get_public_key(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    def get_private_key(self):
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
