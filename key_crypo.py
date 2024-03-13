25349391ffd6f2d5260d1172760a7663d98e86f2
nikhil karmude <nikhilkarmude@XMC02DR14UMD6M>
2024-03-13 18:55:21 -0400

    test

--- key_crypo.py
+++ key_crypo.py
@@ -3,38 +3,37 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-# Generate a RSA Key Pair
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
+class AsymmetricCipher:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        self.public_key = self.private_key.public_key()
+        self.padding = padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+        
+    def encrypt(self, message):
+        encrypted = self.public_key.encrypt(
+            message,
+            self.padding
+        )
+        return encrypted
+        
+    def decrypt(self, encrypted):
+        original_message = self.private_key.decrypt(
+            encrypted,
+            self.padding
+        )
+        return original_message
 
-# Get Public Key from the Private Key
-public_key = private_key.public_key()
-
-# Message to be encrypted
-message = b"A secret message"
-
-# Encryption
-encrypted = public_key.encrypt(
-    message,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-print("Encrypted message:",encrypted)
-
-# Decryption
-original_message = private_key.decrypt(
-    encrypted,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-print("Original message:", original_message)
+# usage example
+cipher = AsymmetricCipher()
+message = b'A secret message'
+encrypted = cipher.encrypt(message)
+print('Encrypted message:', encrypted)
+decrypted = cipher.decrypt(encrypted)
+print('Decrypted message:', decrypted)
